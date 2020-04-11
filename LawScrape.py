f97fb179f9670544ef9873d13bec73c0619ec9d1
d0tus3r <d0tus3r@gmail.com>
2020-04-11 15:17:50 -0400

    parsing url file completed, adds journal name

--- LawScrape.py
+++ LawScrape.py
@@ -1,20 +1,13 @@
-#!/usr/bin/env python3
-
 import urllib.request
 from bs4 import BeautifulSoup
-from sys import argv
 
-# Refactor to pull urls from a text document
-page = urllib.request.urlopen(argv[1])
-html = BeautifulSoup(page.read(), "html.parser")
-
+#All print statements are for proof of concept, will be turned into xml doc upon completion
 
 #open file containing journal urls to parse
 def getURL():
     f = open('urls')
-    urllist = []
+    urlList = []
     for line in f:
-        print("Parsing url: " + line)
         urlList.append(line)
     return urlList
 
@@ -46,6 +39,12 @@ def getNames(author):
     authorNames = author.split(' ')
     return authorNames
 
+def getJournal(url):
+    urlList = url.split("/")
+    if urlList[3] == "elj":
+        journal = "Emory Law Journal"
+        return journal
+
 def getAuthor(author):
     #Daniel B. Rodriguez, Mathew D. McCubbins, Barry R. Weingast
     #check to see if paper has multiple authors
@@ -56,43 +55,56 @@ def getAuthor(author):
     else:
         print(getNames(author))
 #continue work on author names
+# Refactor to pull urls from a text document
+
+def parseDocument(url):
+    #open url from list
+    page = urllib.request.urlopen(url)
+    html = BeautifulSoup(page.read(), "html.parser")
+    rootUrl = getRootUrl(url)
+
+    for link in html.find_all('meta'):
+
+        if link.get('property') == 'og:title':
+            print("Title")
+            print(getTitle(link.get('content')))
+            print("")
+
+        if link.get('name') == 'author':
+            print("Author")
+            getAuthor(link.get('content'))
+            #print(link.get('content'))
+            print("")
+
+        if link.get('property') == 'og:description':
+            print("Abstract")
+            print(link.get('content'))
+            print("")
+
+        if link.get('name') == 'start-page-number':
+            print("First Page")
+            print(link.get('content'))
+            print("")
+
+    for link in html.find_all('a'):
+        pdfUrl = link.get('href')
+        if "pdf" in pdfUrl:
+            newUrl = stripPdfUrl(pdfUrl)
+            print("PDF Url")
+            print(rootUrl + newUrl)
+
+    print("") #temp line break for readability
+    #print("Document Type")
+    #print
+
+    getVolumeIssue(url)
+
+    print("Journal")
+    journal = getJournal(url)
+    print(journal)
 
 
-rootUrl = getRootUrl(argv[1])
+urlList = getURL()
 
-
-for link in html.find_all('meta'):
-
-    if link.get('property') == 'og:title':
-        print("Title")
-        print(getTitle(link.get('content')))
-        print("")
-
-    if link.get('name') == 'author':
-        print("Author")
-        getAuthor(link.get('content'))
-        #print(link.get('content'))
-        print("")
-
-    if link.get('property') == 'og:description':
-        print("Abstract")
-        print(link.get('content'))
-        print("")
-
-    if link.get('name') == 'start-page-number':
-        print("First Page")
-        print(link.get('content'))
-        print("")
-
-for link in html.find_all('a'):
-    pdfUrl = link.get('href')
-    if "pdf" in pdfUrl:
-        newUrl = stripPdfUrl(pdfUrl)
-        print("PDF Url")
-        print(rootUrl + newUrl)
-
-print("") #temp line break for readability
-#print("Document Type")
-#print
-
-getVolumeIssue(argv[1])
+for url in urlList:
+    parseDocument(url)
